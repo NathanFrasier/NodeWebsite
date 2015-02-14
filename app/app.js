8b72e6e7d08ff496c4bf7c582314a8a11f248780
Nathan Frasier <frasiern@msoe.edu>
2015-02-14 15:19:47 -0600

    added missing semicolons and such

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
-var express = require('express')
+var express = require('express');
 
-var session = require('express-session')
-var MongoStore = require('connect-mongo')(session)
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
-var auth = require('./auth')
+var auth = require('./auth');
 
-var app = express()
+var app = express();
 
 //every page starts a session
 app.use(session({
@@ -17,36 +17,36 @@ app.use(session({
 	store: new MongoStore({
 		url: 'mongodb://localhost/NodeWebsite'
 	})
-}))
+}));
 
 //every page has its body parsed
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 //routing for the root page
 app.get('/', function (req, res) {
-  res.redirect('/index')
-})
+  res.redirect('/index');
+});
 
 //routingg for the /index page
 app.get('/index', function (req, res) {
-  res.send('<p>Hello World!</p>')
-})
+  res.send('<p>Hello World!</p>');
+});
 
 //routing for the /home page
 app.get('/home',auth.authenticate, function (req, res)
 {
-	res.send('Woot! You made it to the homepage! Wanna head on over to <a href="https://www.reddit.com">Reddit?</a>')
-})
+	res.send('Woot! You made it to the homepage! Wanna head on over to <a href="https://www.reddit.com">Reddit?</a>');
+});
 
 app.get('/login', function (req, res){
-	res.send('<form action="/login" method="POST">Username:<br/><input type="text" name="username"><br/>password:<br/><input type="password" name="passwd"><br/><input type="submit" value="login"/></form>')
-})
+	res.send('<form action="/login" method="POST">Username:<br/><input type="text" name="username"><br/>password:<br/><input type="password" name="passwd"><br/><input type="submit" value="login"/></form>');
+});
 
-app.post('/login', auth.login)
+app.post('/login', auth.login);
 
 app.use(function(req, res){
-	res.status(404).send("Whoops! I dont know a page by that name.")
-})
+	res.status(404).send("Whoops! I dont know a page by that name.");
+});
 
-module.exports = app
+module.exports = app;
--- app/auth.js
+++ app/auth.js
@@ -1,47 +1,47 @@
-var MongoClient = require('mongodb').MongoClient
-var auth = {}
+var MongoClient = require('mongodb').MongoClient;
+var auth = {};
 
 auth.authenticate = function(req, res, next){
 	if(req.session.loggedin) {
-		next()
+		next();
 	} else {
-		res.statusCode = 403
-		res.send('<h1>Sorry!</h1><p>You are not authenticated.</p><br/><p>You need to be <a href="/login">logged in</a> to access this page.<p>')
+		res.statusCode = 403;
+		res.send('<h1>Sorry!</h1><p>You are not authenticated.</p><br/><p>You need to be <a href="/login">logged in</a> to access this page.<p>');
 	}
-}
+};
 
 auth.login = function (req, res){
 	auth.validCredentials(req.body.username,req.body.passwd, function(isValid){
 		if(isValid) {
-			req.session.loggedin = true
-			res.send('You have sucessfully logged in. Welcome!')
+			req.session.loggedin = true;
+			res.send('You have sucessfully logged in. Welcome!');
 		} else {
-			res.send('Your login has failed, are you <a>registered?</a>')	
+			res.send('Your login has failed, are you <a>registered?</a>');
 		}
-	})
-}
+	});
+};
 auth.validCredentials = function(username, passwd, callback){
 	MongoClient.connect('mongodb://localhost/NodeWebsite', function (err, db) {
 		if(err) {
-			return console.dir(err)
+			return console.dir(err);
 		}
-		var collection = db.collection('users')
+		var collection = db.collection('users');
 		collection.findOne({'username':username},{'passwd':1},function(err, result){
 			if(err) {
-				return console.dir(err)
+				return console.dir(err);
 			}
 			if(result == null){
-				callback(false)
+				callback(false);
 			} else {
 				if(result.passwd == passwd) {
-					callback(true)
+					callback(true);
 				} else {
-					callback(false)
+					callback(false);
 				}
 			}
 			
-		})
-	})
-}
+		});
+	});
+};
 
-module.exports = auth
+module.exports = auth;
